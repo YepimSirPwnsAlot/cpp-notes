c41c3ee99ae245f1745eb4671ad87f7df7a2744b
YepImNythic <guest45542008@gmail.com>
2026-05-14 21:06:09 -0400

    Update Variables.cpp

--- Variables.cpp
+++ Variables.cpp
@@ -1,9 +1,12 @@
 // Now we will talk about variables, variables are important because they can store values. The variable could be a: string, number, boolean, or other C++ data types. An example of a variable would be:
-/* int myNumber = 10; double pi = 3.14;
+/* int myNumber = 10; double pi = 3.14; 
+   float myPrice = 9.99f;
    char myLetter = 'A';
    bool isTrue = true;
    std::string myName = "John"; 
 */
+
+// Float is also important because it is essential for handling decimal precision in functional programming, data analysis, and mathematical modeling. While integers are limited to whole numbers, floats allow functions to return exact fractional results (e.g.,) 
 
 // If you really want to use a variable then you would type:
 #include <iostream>
@@ -14,6 +17,7 @@ int main() {
     // 1. Integers and Decimals
     int myNumber = 10; 
     double pi = 3.14;
+    float myPrice = 19.99f;
 
     // 2. Characters (Single quotes)
     char myLetter = 'A';
@@ -25,7 +29,7 @@ int main() {
     std::string myName = "John";
 
     // Let's actually see the output!
-    std::cout << myName << " says pi is " << pi << std::endl;
+    std::cout << myName << " says the price is " << myPrice << std::endl;
 
     return 0;
 }

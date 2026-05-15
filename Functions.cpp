3d02f52d70d0f71fb5c257b27ecd61918353f465
YepImNythic <guest45542008@gmail.com>
2026-05-14 20:36:36 -0400

    Correct function call in main
    Fix typo in function call from 'great' to 'greet'.

---
+++ Functions.cpp
@@ -0,0 +1,15 @@
+// Ok so now is functions
+// Functions in C++ are reusable blocks of code that perform specific tasks, helping you organize your program and avoid repetition.
+// Basic Function (No Return Value) A simple function can perform an action, such as printing a message, without returning any data to the caller. You use the void keyword to indicate there is no return value.
+
+#include <iostream>
+using namespace std;
+
+void greet() {
+	std::cout << "Hello, World!";
+}
+
+int main() {
+	great(); // This calls the function.
+	return 0;
+}
